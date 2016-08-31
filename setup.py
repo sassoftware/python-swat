78d7ace6908b901487711d0af47798e91315a795
Kevin D Smith <kevin.smith@sas.com>
2016-08-31 15:22:32 -0400

    Disable messaging; it will not work in pip anyway

--- setup.py
+++ setup.py
@@ -16,73 +16,31 @@
 #  limitations under the License.
 #
 
-''' Install the SWAT module '''
+''' Install the SAS Wrapper for Analytics Transfer (SWAT) module '''
 
-from setuptools import setup, find_packages
-from setuptools.command.install import install
 import glob
-import os
-import struct
-import sys
+from setuptools import setup, find_packages
 
 try:
-    input = raw_input
+    README = open('README.rst', 'r').read()
 except:
-    pass
+    README = 'See README.rst'
 
-
-def has_tk():
-    ''' See if TK components exist '''
-    return len(glob.glob('swat/lib/*/*')) > 10
-
-
-def accept_license():
-    ''' Display TK license and check for acceptance '''
-    import pydoc
-    os.environ['LESS'] = os.environ.get('LESS', '') + ' -e'
-    pydoc.pager(open(os.path.join('LICENSES', 'SAS-TK.txt'), 'r').read())
-    out = input('Do you accept the terms of the license? [y/N] ')
-    if out.lower().strip().startswith('y'):
-        return True
-    return False
-
-
-class SWATInstaller(install):
-    ''' Make sure that the Python executable is 64-bit '''
-    def run(self):
-        size = struct.calcsize('P')  # integer size
-        if size != 8:
-            print('')
-            print('ERROR: Sorry, you must have 64bit Python installed.')
-            print('       This version of Python is %dbit:' % (size*8))
-            print('')
-            #print(sys.version)
-            raise Exception('This packages requires 64bit Python')
-        if os.environ.get('CONDA_BUILD', None) or os.environ.get('WHEEL_BUILD', None):
-            install.run(self)
-        elif os.environ.get('ACCEPT_SAS_TK_LICENSE', '').lower().startswith('y'):
-            install.run(self)
-        elif not has_tk():
-            print('')
-            print('NOTE: Only the REST interface is supported with the pure Python installation.')
-            print('      Currently, only Linux installations support the binary protocol.')
-            print('      Use the pip or conda installers for binary protocol support.')
-            print('')
-            install.run(self)
-        elif has_tk() and accept_license():
-            install.run(self)
+if glob.glob('swat/lib/*/tk*'):
+    LICENSE = 'Apache v2.0 + SAS Additional Functionality'
+else:
+    LICENSE = 'Apache v2.0'
 
 setup(
-    cmdclass = {'install': SWATInstaller},
     zip_safe = False,
     name = 'swat',
     version = '1.0.0',
     description = 'SAS Wrapper for Analytics Transfer (SWAT)',
-    long_description = open('README.rst', 'r').read(),
-    author = 'Kevin D Smith',
+    long_description = README,
+    author = 'SAS',
     author_email = 'Kevin.Smith@sas.com',
     url = 'http://github.com/sassoftware/python-swat/',
-    license = 'LICENSE/SWAT.txt',
+    license = LICENSE,
     packages = find_packages(),
     package_data = {
         'swat': ['lib/*/*'],
@@ -93,16 +51,15 @@ setup(
         'requests',
     ],
     classifiers = [
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'License :: Other/Proprietary License',
-        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
     ],
 )
