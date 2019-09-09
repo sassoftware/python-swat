97db8b25b20cda3bbba307c614618d92fbb5ce0d
Kevin D Smith <kevin.smith@sas.com>
2019-09-09 10:01:54 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def get_file(fname):
 setup(
     zip_safe=False,
     name='swat',
-    version='1.5.2-dev',
+    version='1.5.2',
     description='SAS Scripting Wrapper for Analytics Transfer (SWAT)',
     long_description=get_file('README.md'),
     long_description_content_type='text/markdown',
@@ -63,6 +63,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
     ],
 )
