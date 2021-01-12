30b66f43e790511de04361f7249a2892c080b7fc
Kevin D Smith <kevin.smith@sas.com>
2021-01-12 13:15:24 -0600

    Increment development version

--- swat/__init__.py
+++ swat/__init__.py
@@ -93,5 +93,5 @@ from .exceptions import SWATError, SWATOptionError, SWATCASActionError    # noqa
 # SAS Formatter
 from .formatter import SASFormatter    # noqa: E402
 
-__version__ = '1.8.0'
+__version__ = '1.8.1-dev'
 __tk_version__ = None
